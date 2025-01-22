e6018ff9db9a21fe8202756cecef8e9d19b35ec3
Tomás Martín <tmartindsgn@gmail.com>
2025-01-22 21:03:39 +0100

    dotenv & mongoose

--- server/app.js
+++ server/app.js
@@ -2,12 +2,18 @@ const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
+const mongoose = require("mongoose")
+require("dotenv").config();
+const MONGODB_URI = process.env.MONGODB_URI;
+mongoose
+  .connect(MONGODB_URI)
+  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .catch(err => console.error("Error connecting to mongo", err));
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 const students = require("./students.json");
 const cohorts = require("./cohorts.json")
-
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
