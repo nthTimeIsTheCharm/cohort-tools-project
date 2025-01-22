7ad91478b52fcc4f493c7e5272268fb5bf8d7d6c
Tomás Martín <tmartindsgn@gmail.com>
2025-01-22 21:34:32 +0100

    starting models

---
+++ server/models/Student.model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+// definimos el esquema de la colección
+const studentSchema = new Schema({
+    firstName:{
+        type: String
+        required: [true, "Fisrt name is requiered"],
+    }
+
+    firstName:{
+
+    }
+}
+    
+);
+  
